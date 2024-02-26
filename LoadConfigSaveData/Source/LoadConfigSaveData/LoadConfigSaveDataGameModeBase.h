345c2bba5425ac607f0ff3d2141dd2724072e965
KIPKIPS <2432719656@qq.com>
2024-02-26 20:03:41 +0800

    工程初始化

---
+++ LoadConfigSaveData/Source/LoadConfigSaveData/LoadConfigSaveDataGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LoadConfigSaveDataGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOADCONFIGSAVEDATA_API ALoadConfigSaveDataGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
